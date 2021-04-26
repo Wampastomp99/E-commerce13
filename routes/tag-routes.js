38137289898b7a534484d3395c59cc232e54b17d
michael oreilly <michaelheartbound99@hotmail.com>
2021-04-25 20:42:12 -0600

    finnished tag routes

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -55,9 +55,9 @@ router.get("/:id", (req, res) => {
       });
   });
   
-  // POST ROUTE
+  
   router.post("/", (req, res) => {
-    // Create a new tag
+    
     Tag.create({
       tag_name: req.body.tag_name,
     })
@@ -67,3 +67,52 @@ router.get("/:id", (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  
+router.put("/:id", (req, res) => {
+    
+    Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbTagData) => {
+        if (!dbTagData[0]) {
+          res
+            .status(404)
+            .json({ message: 'There was no tag found with this id.' });
+          return;
+        }
+        res.json(dbTagData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  
+  router.delete("/:id", (req, res) => {
+    
+    Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbTagData) => {
+        if (!dbTagData) {
+          res
+            .status(404)
+            .json({ message: 'There was no tag found with this id.' });
+          return;
+        }
+        res.json(dbTagData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  module.exports = router;
+  
