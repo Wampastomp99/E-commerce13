8078d61dbffbec51b78bbc951a5b37d16eca7c1e
michael oreilly <michaelheartbound99@hotmail.com>
2021-04-25 20:35:59 -0600

    more routes

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -23,3 +23,47 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
+router.get("/:id", (req, res) => {
+    
+    Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"],
+          through: ProductTag,
+          as: "products",
+        },
+      ],
+    })
+      .then((dbTagData) => {
+        if (!dbTagData) {
+          res
+            .status(404)
+            .json({ message: "There was no tag found with this id." });
+          return;
+        }
+        res.json(dbTagData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  // POST ROUTE
+  router.post("/", (req, res) => {
+    // Create a new tag
+    Tag.create({
+      tag_name: req.body.tag_name,
+    })
+      .then((dbTagData) => res.json(dbTagData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
